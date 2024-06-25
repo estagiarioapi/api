a810da48578083f9009ec1eb62af5e2726af3579
Otávio <ocostadallo@gmail.com>
2024-06-25 16:41:21 -0300

    adding route

--- src/fluxo/fluxo.service.ts
+++ src/fluxo/fluxo.service.ts
@@ -1,55 +1,65 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class FluxoService {
-  async sendInteractiveMessage(phoneNumber: string) {
-    return {
-      recipient_type: 'individual',
-      to: phoneNumber,
-      type: 'interactive',
-      interactive: {
-        type: 'list',
-        header: {
-          type: 'text',
-          text: 'Funcionalidades',
+  constructor(private httpService: HttpService) { }
+
+  async sendInteractiveMessage(phoneNumber: string): Promise<AxiosResponse<any>> {
+    const message = {
+      "recipient_type": "individual",
+      "to": phoneNumber,
+      "messaging_product": "whatsapp",
+      "type": "interactive",
+      "interactive": {
+        "type": "list",
+        "body": {
+          "text": "Olá chefe, como posso te ajudar?"
         },
-        body: {
-          text: 'your-text-message-content',
-        },
-        footer: {
-          text: 'your-footer-content',
-        },
-        action: {
-          button: 'cta-button-content',
-          sections: [
+        "action": {
+          "button": "Funcionalidades",
+          "sections": [
             {
-              title: '',
-              rows: [
+              "title": "",
+              "rows": [
                 {
-                  id: '1',
-                  title: 'Auxiliar Jurídico',
-                  description: 'row-description-content',
+                  "id": "1",
+                  "title": "Auxiliar Jurídico"
                 },
                 {
-                  id: '2',
-                  title: 'Notif. Extrajudicial',
-                  description: 'row-description-content',
+                  "id": "2",
+                  "title": "Notificação Extrajudicial"
                 },
                 {
-                  id: '3',
-                  title: 'Peças Processuais',
-                  description: 'row-description-content',
+                  "id": "3",
+                  "title": "Peças Processuais"
                 },
                 {
-                  id: '4',
-                  title: 'Contratos',
-                  description: 'row-description-content',
-                },
-              ],
-            },
-          ],
-        },
-      },
+                  "id": "4",
+                  "title": "Contratos"
+                }
+              ]
+            }
+          ]
+        }
+      }
     };
+
+    try {
+      const url = 'https://graph.facebook.com/v19.0/374765715711006/messages';
+      const headers = {
+        'Authorization': 'Bearer EAARMCGe1MUcBOzNDrytFoDd0aad954W7PocOQa10C8cxGsB35ZBlx71Mt5Dmyl8PhCN68FffPOuAhrXIGQtZAAr8Q9Cf3fpOsoCtGah2ZBVHAU5n8aTVQCR3d9hTsQbccnBpKVVXYR7ZCOnrocsJIiBAIBAPayRqrRMGShoxYTHZBVeA6JJnnHcXjTwCZBe0dcOKuLVuHUKlI6JBsKI1sZD',
+        'Content-Type': 'application/json'
+      };
+      const response = await firstValueFrom(
+        this.httpService.post(url, message, { headers })
+      );
+      return response.data
+    } catch (error) {
+      console.error('Error sending message:', error);
+      throw error;
+    }
   }
 }
